2020a17150084d81adbc38e286f90c11fa48b689
Adit-Vij <aditvij2004@gmail.com>
2024-08-28 12:58:41 +0530

    Update KeyGen.py

--- Scripts/keyGen.py
+++ Scripts/keyGen.py
@@ -15,8 +15,20 @@ class RSAKeyGen:
         self.KEY_LENGTH = 4096
     
     def generate_keys(self):
-         private_key = rsa.generate_private_key(
+        private_key = rsa.generate_private_key(
             public_exponent=65537,
-            key_size=self.key_size,
-            backend=default_backend()
-        )
+            key_size=self.KEY_LENGTH,
+            backend=default_backend())
+        public_key = private_key.public_key()
+        return private_key, public_key
+
+    def private_key_to_pem(self, private_key):
+        pem = private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                        format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                        encryption_algorithm=serialization.NoEncryption)
+        return pem
+
+    def public_key_to_pem(self, public_key):
+        pem = public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                        format=serialization.PublicFormat.SubjectPublicKeyInfo)
+        return pem
